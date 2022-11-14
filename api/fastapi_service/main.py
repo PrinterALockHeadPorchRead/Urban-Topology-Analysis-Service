490ee622ebc1da33bb754a0d6bdf60bdf121ac6f
Ermolaefff <fallen2320@yandex.ru>
2022-11-15 02:59:02 +0300

    localhost acces

--- api/fastapi_service/main.py
+++ api/fastapi_service/main.py
@@ -15,7 +15,7 @@ logger = logs.init()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:4200", "http://localhost:8002"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
